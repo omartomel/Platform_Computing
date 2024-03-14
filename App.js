d769a947050fd465eab7c274805abed58c1cbf58
omartomel <159991525+omartomel@users.noreply.github.com>
2024-03-13 21:02:56 -0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,67 @@
+import logo from './logo.svg';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+
+
+	<head>
+<html>
+<title>
+About Me - Omar Elizalde
+</title>
+</html>
+	</head>
+        <p>
+<html>
+
+          <h1> Omar Elizalde's Website </h1>
+        This is the website html for the CSUSB student Omar Elizalde for his CSE 4500 class.
+        <div>Do you like it?</div>
+
+<div class="measf">
+            <img src="https://i.imgur.com/IfYXaT6.png" alt="Elliott photo." style={{marginRight: 'spacing' + 'em'}} class="center" />
+          </div>
+
+            <p>I don't have many photos of me. Here's me at the beach last summer. It was a cold day though.</p>
+		<h2> About Me</h2>
+Omar Elizalde is many things. His traits can be summarized in a bullet list below.
+        Not sure how to get the actual bullets in the center but it shall work for now!
+
+            <li>CSUSB Student</li>
+            <li>Computer Science Major</li>
+            <li>Video Game Lover</li>
+            <li>Aspiring Artist</li>
+            <li>Youtube Addict</li>
+            <li>Proud Pet Owner</li>
+            <li><a href="https://github.com/omartomel">Github Link</a></li>
+
+<div class="bunny">
+            <img src="https://i.imgur.com/iWkXmXf.png" alt="Elliott photo." style={{marginRight: 'spacing' + 'em'}} class="center" />
+            <p>Elliott (2016-2023)</p>
+            <p>
+                Elliott was one of my favorite pets, I picked him myself unlike my other ones, and he had such a unique personality that I don't think anybody else understood.
+            </p>
+          </div>
+
+</html>
+	</p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Test
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
